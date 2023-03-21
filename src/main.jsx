b1fe51d60c2cd355c32e3cf4da73d80ac70a0782
Leo <leo07carrasco@gmail.com>
2023-03-21 13:16:58 -0300

    scroll to top

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,28 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { useEffect } from 'react'
+
+import { useLocation } from "react-router-dom";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    document.documentElement.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "instant",
+    });
+  }, [pathname]);
+
+  return null;
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
     <BrowserRouter>
+        <ScrollToTop />
         <App />
     </BrowserRouter>
 
